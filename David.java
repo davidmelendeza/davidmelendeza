bf06ae41ab28974d65c610317a4a408aaf7dd4e6
David Melendez <david.melendezaguilar@ucr.ac.cr>
2021-04-26 03:01:40 -0600

    Primer commit del repositorio

---
+++ David.java
@@ -0,0 +1,6 @@
+public class David{
+    public static void main(String[] args){
+        System.out.println("Mi nombre es david");
+        System.out.println("Mi comida favorita es la pizza");
+    }
+}
